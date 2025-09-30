1e0b398a42ed6378779a8c991c17e0faf7ee0549
SornchaiTheDev <sornchaithedev@gmail.com>
2025-10-01 00:05:59 +0700

    feat(course): update create course schema

--- src/app/(authed)/cms/courses/_schemas/course.create.ts
+++ src/app/(authed)/cms/courses/_schemas/course.create.ts
@@ -3,7 +3,7 @@ import { userDataSchema } from "~/schemas/user-data.schema";
 
 export const createCourseSchame = z.object({
   name: z.string().min(1, "Course name is required"),
-  creators: userDataSchema.min(1, "Creator is required"),
+  creators: z.array(userDataSchema).min(1, "Creator is required"),
   type: z.enum(["public", "private"]).default("public"),
 });
 
