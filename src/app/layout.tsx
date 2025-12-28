ba7493ad4dd2600c152967ce903dd2e37babd8a3
SornchaiTheDev <sornchaithedev@gmail.com>
2025-12-28 12:23:54 +0700

    feat: implement gradebook page ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Anuphan, Poppins } from "next/font/google";
+import localFont from "next/font/local";
 import AppLoading from "~/components/commons/AppLoading";
 import { Suspense } from "react";
 import { ClientEnv } from "~/lib/client-env";
@@ -17,6 +18,22 @@ const anuphan = Anuphan({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700"],
   variable: "--font-anuphan",
+});
+
+const boon = localFont({
+  src: [
+    { path: "../assets/fonts/Boon-Regular.ttf", weight: "400", style: "normal" },
+    { path: "../assets/fonts/Boon-Bold.ttf", weight: "700", style: "normal" },
+    { path: "../assets/fonts/Boon-Italic.ttf", weight: "400", style: "italic" },
+    { path: "../assets/fonts/Boon-BoldItalic.ttf", weight: "700", style: "italic" },
+    { path: "../assets/fonts/Boon-Light.ttf", weight: "300", style: "normal" },
+    { path: "../assets/fonts/Boon-LightItalic.ttf", weight: "300", style: "italic" },
+    { path: "../assets/fonts/Boon-Medium.ttf", weight: "500", style: "normal" },
+    { path: "../assets/fonts/Boon-MediumItalic.ttf", weight: "500", style: "italic" },
+    { path: "../assets/fonts/Boon-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "../assets/fonts/Boon-SemiBoldItalic.ttf", weight: "600", style: "italic" },
+  ],
+  variable: "--font-boon",
 });
 
 export const metadata: Metadata = {
@@ -48,7 +65,7 @@ export default function RootLayout({
       <head>
         <ClientEnv />
       </head>
-      <body className={`${onest.className} ${anuphan.variable}`}>
+      <body className={`${onest.className} ${anuphan.variable} ${boon.variable}`}>
         <ReactScan />
         <Suspense>
           <AppLoading />
