1f44e215b8d45a30e19b4f7f55f501f8972fc9a2
SornchaiTheDev <sornchaithedev@gmail.com>
2026-02-18 17:26:27 +0700

    feat: update section labs page

--- src/types/cms-section-lab.ts
+++ src/types/cms-section-lab.ts
@@ -1,9 +1,12 @@
+export type LabStatus = "hidden" | "open" | "readonly" | "disabled" | "closed";
+
 export interface CMSSectionLab {
   id: string;
   lab_name: string;
   section_id: string;
   lab_id: string;
   position: number;
+  status: LabStatus;
   created_at: Date;
   updated_at: Date;
 }
