1945cd09309c162844edeb7373bea505c2eed195
SornchaiTheDev <sornchaithedev@gmail.com>
2026-02-24 01:04:02 +0700

    feat: add border to runner detail page

--- src/app/(authed)/cms/(full-height-layout)/runners/[runnerId]/_components/DetailSection/index.tsx
+++ src/app/(authed)/cms/(full-height-layout)/runners/[runnerId]/_components/DetailSection/index.tsx
@@ -10,7 +10,7 @@ function DetailSection() {
   const { data: runner, isLoading } = useRunner(runnerId);
 
   return (
-    <div className="border border-l-0 2xl:border-l pl-4 pr-2 py-3 w-full flex items-center justify-between gap-4 mt-4">
+    <div className="border-b pl-4 pr-2 py-3 w-full flex items-center justify-between gap-4">
       <div className="flex-1 flex gap-4 overflow-x-auto">
         <HeaderItem label="Name" value={runner?.name} isLoading={isLoading} />
         <HeaderItem
