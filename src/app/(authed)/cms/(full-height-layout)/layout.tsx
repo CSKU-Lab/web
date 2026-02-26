20b255389e0af630efa86fd58ff40ce393ddc6c0
xeff09 <wongchaita03@gmail.com>
2026-02-26 17:39:46 +0700

    mod: instructor's ai assistant chat panel into sidebar

--- src/app/(authed)/cms/(full-height-layout)/layout.tsx
+++ src/app/(authed)/cms/(full-height-layout)/layout.tsx
@@ -9,24 +9,27 @@ import { BreadcrumbProvider } from "../_components/BreadcrumbProvider";
 import SidebarMenus from "../_components/SidebarMenus";
 import { getUser } from "~/lib/get-user";
 import { getSidebarMenus } from "../_configs/sidebar-menus";
+import ChatProvider from "~/providers/ChatProvider";
 
 async function Layout({ children }: ChildrenProps) {
   const user = await getUser();
   const sidebarMenus = getSidebarMenus(user.roles);
 
   return (
-    <CoreLayout homePath="/cms">
-      <CoreLayoutSidebar>
-        <h6 className="text-(--gray-11) text-xs font-light py-2">CMS</h6>
-        <SidebarMenus config={sidebarMenus} />
-      </CoreLayoutSidebar>
-      <CoreLayoutContent className="h-full">
-        <BreadcrumbProvider>
-          <BreadcrumbClient className="pl-4 mt-4" />
-          {children}
-        </BreadcrumbProvider>
-      </CoreLayoutContent>
-    </CoreLayout>
+    <ChatProvider>
+      <CoreLayout homePath="/cms">
+        <CoreLayoutSidebar>
+          <h6 className="text-(--gray-11) text-xs font-light py-2">CMS</h6>
+          <SidebarMenus config={sidebarMenus} />
+        </CoreLayoutSidebar>
+        <CoreLayoutContent className="h-full">
+          <BreadcrumbProvider>
+            <BreadcrumbClient className="pl-4 mt-4" />
+            {children}
+          </BreadcrumbProvider>
+        </CoreLayoutContent>
+      </CoreLayout>
+    </ChatProvider>
   );
 }
 
