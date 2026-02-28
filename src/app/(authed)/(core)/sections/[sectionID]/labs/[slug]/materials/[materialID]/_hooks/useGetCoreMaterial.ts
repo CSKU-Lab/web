664c141de29ffca2f50c1d572f2e279e7b5c2734
xeff09 <wongchaita03@gmail.com>
2026-02-28 18:49:39 +0700

    fix: invalid core material call & add: context based system prompt into student's ai

--- src/app/(authed)/(core)/sections/[sectionID]/labs/[slug]/materials/[materialID]/_hooks/useGetCoreMaterial.ts
+++ src/app/(authed)/(core)/sections/[sectionID]/labs/[slug]/materials/[materialID]/_hooks/useGetCoreMaterial.ts
@@ -4,11 +4,16 @@ import { useParams } from "next/navigation";
 import { coreMaterialService } from "~/services/core-material.service";
 
 function useGetCoreMaterial<T>() {
-  const { materialID } = useParams<{ materialID: string }>();
+  const {
+    materialID,
+    sectionID,
+    slug: labID,
+  } = useParams<{ materialID: string; sectionID: string; slug: string }>();
 
   return useQuery({
     queryKey: queryKeys.core.material.getById(materialID),
-    queryFn: async () => coreMaterialService.getById<T>(materialID),
+    queryFn: async () =>
+      coreMaterialService.getById<T>(materialID, sectionID, labID),
     placeholderData: keepPreviousData,
   });
 }
--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -3,23 +3,38 @@ import { chatInstance as instructor } from "~/lib/aiChatProviders/instructor.cha
 import { chatInstance as student } from "~/lib/aiChatProviders/student.chat";
 import { getUser } from "~/lib/get-user";
 
+export interface ProblemProps {
+  materialID: string;
+  labID: string;
+  sectionID: string;
+}
+
 export async function POST(req: Request) {
-  const { messages }: { messages: UIMessage[] } = await req.json();
+  const {
+    messages,
+    probIDs,
+  }: {
+    messages: UIMessage[];
+    probIDs?: ProblemProps;
+  } = await req.json();
+
   const user = await getUser();
   const hasToolAccess = user.roles.some((role) =>
     ["admin", "instructor"].includes(role),
   );
 
   if (hasToolAccess) {
-    const res = await instructor.getStream({
+    return instructor.getStream({
       messages,
     });
-
-    return res;
   }
 
-  const res = await student.getStream({
+  if (!probIDs) {
+    return;
+  }
+
+  return student.getStream({
     messages,
+    probIDs,
   });
-  return res;
 }
