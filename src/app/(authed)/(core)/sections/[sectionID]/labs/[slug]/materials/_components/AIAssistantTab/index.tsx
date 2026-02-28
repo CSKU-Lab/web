9e85cddc9337f878851b19ae94a0acfcad26aebc
xeff09 <wongchaita03@gmail.com>
2026-02-28 19:18:59 +0700

    connect: problem context call on student ai instance

--- src/app/(authed)/(core)/sections/[sectionID]/labs/[slug]/materials/_components/AIAssistantTab/index.tsx
+++ src/app/(authed)/(core)/sections/[sectionID]/labs/[slug]/materials/_components/AIAssistantTab/index.tsx
@@ -1,9 +1,23 @@
+import { useParams } from "next/navigation";
+import { ProblemProps } from "~/app/api/chat/route";
 import ChatPanel from "~/components/commons/ChatPanel";
 
 export default function AIAssistantTab() {
+  const {
+    materialID,
+    sectionID,
+    slug: labID,
+  } = useParams<{ materialID: string; sectionID: string; slug: string }>();
+
+  const probIDs: ProblemProps = {
+    materialID,
+    sectionID,
+    labID,
+  };
+
   return (
     <div className="flex flex-col h-full">
-      <ChatPanel />
+      <ChatPanel probIDs={probIDs} />
     </div>
   );
 }
