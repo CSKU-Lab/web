4dd798a0489058849c14a7e7aebde3a3706c20ff
SornchaiTheDev <sornchaithedev@gmail.com>
2026-03-01 15:42:38 +0700

    feat: implement Runners Tab

--- src/app/(authed)/cms/(full-height-layout)/materials/[materialID]/_materialTypes/CodeMaterial/_types/code-material-payload.ts
+++ src/app/(authed)/cms/(full-height-layout)/materials/[materialID]/_materialTypes/CodeMaterial/_types/code-material-payload.ts
@@ -1,11 +1,13 @@
 import type { CodeMaterialSolutionFile } from "./file";
 import type { CodeMaterialLimit } from "./limit";
 import type { TestCaseGroup } from "./testcase-group";
+import type { RunnerTemplatePayload } from "../_components/RunnersTab/_types/runner-template";
 
 export interface CodeMaterialPayload {
   description: string;
   test_case_groups: TestCaseGroup[];
   allowed_runner_ids: string[];
+  allowed_runner_templates: RunnerTemplatePayload[];
   compare_script_id: string | null;
   solution_runner_id: string | null;
   solution_files: CodeMaterialSolutionFile[];
