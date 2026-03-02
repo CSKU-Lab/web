ac368a859a08e07ff2810bfd89172eca3f2e1089
xeff09 <wongchaita03@gmail.com>
2026-03-02 10:08:36 +0700

    move: messages declaration onto upper layer

--- src/app/(authed)/(core)/sections/[sectionID]/labs/[slug]/materials/_components/AIAssistantTab/index.tsx
+++ src/app/(authed)/(core)/sections/[sectionID]/labs/[slug]/materials/_components/AIAssistantTab/index.tsx
@@ -1,30 +1,12 @@
 import { useChat } from "@ai-sdk/react";
-import { DefaultChatTransport } from "ai";
-import { useParams } from "next/navigation";
-import { ProblemProps } from "~/app/api/chat/route";
 import ChatPanel from "~/components/commons/ChatPanel";
 
-export default function AIAssistantTab() {
-  const {
-    materialID,
-    sectionID,
-    slug: labID,
-  } = useParams<{ materialID: string; sectionID: string; slug: string }>();
-
-  const probIDs: ProblemProps = {
-    materialID,
-    sectionID,
-    labID,
-  };
-
-  const { messages, sendMessage, status } = useChat({
-    transport: new DefaultChatTransport({
-      api: "/api/chat",
-      body: {
-        probIDs,
-      },
-    }),
-  });
+export default function AIAssistantTab({
+  chat,
+}: {
+  chat: ReturnType<typeof useChat>;
+}) {
+  const { messages, sendMessage, status } = chat;
 
   return (
     <div className="flex flex-col h-full">
