44f3e709529c7df79a1c226aef9b9af7c77359d7
SornchaiTheDev <sornchaithedev@gmail.com>
2026-05-09 20:46:26 +0700

    chore: separate dev config with the production one

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,12 @@
 import type { NextConfig } from "next";
+import { RemotePattern } from "next/dist/shared/lib/image-config";
 
-const nextConfig: NextConfig = {
-  pageExtensions: ["js", "jsx", "ts", "tsx"],
-  transpilePackages: ["next-mdx-remote"],
-  output: "standalone",
-  poweredByHeader: false,
-  images: {
-    dangerouslyAllowLocalIP: true, // need to be disable in production
-    remotePatterns: [
-      {
-        hostname: "lh3.googleusercontent.com",
-      },
+const env = process.env.NODE_ENV;
+
+const isDev = env === "development";
+
+const remotePatterns: RemotePattern[] = isDev
+  ? [
       {
         hostname: "yt3.ggpht.com",
       },
@@ -20,7 +16,21 @@ const nextConfig: NextConfig = {
         port: "9000",
         pathname: "/cs-lab/**",
       },
-    ],
+    ]
+  : [
+      {
+        hostname: "lh3.googleusercontent.com",
+      },
+    ];
+
+const nextConfig: NextConfig = {
+  pageExtensions: ["js", "jsx", "ts", "tsx"],
+  transpilePackages: ["next-mdx-remote"],
+  output: "standalone",
+  poweredByHeader: false,
+  images: {
+    dangerouslyAllowLocalIP: isDev, // need to be disable in production
+    remotePatterns,
   },
 };
 
