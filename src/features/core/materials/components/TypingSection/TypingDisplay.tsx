defdd7281577f8a00dab492f6ee8e3bc2d1c765b
SornchaiTheDev <sornchaithedev@gmail.com>
2026-05-30 20:17:44 +0700

    fix: active text color in typing material

--- src/features/core/materials/components/TypingSection/TypingDisplay.tsx
+++ src/features/core/materials/components/TypingSection/TypingDisplay.tsx
@@ -19,9 +19,9 @@ export default function TypingDisplay({ chars, currentIndex }: Props) {
             c.status === "pending" && "text-(--gray-8)",
             c.status === "correct" && "text-green-400",
             c.status === "incorrect" && "text-red-400",
-            c.status === "current" && "text-white",
+            c.status === "current" && "text-(--gray-12)",
             c.status === "current" &&
-              "after:absolute after:left-0 after:-bottom-0.5 after:w-full after:h-0.5 after:bg-white after:animate-pulse",
+              "after:absolute after:left-0 after:-bottom-0.5 after:w-full after:h-0.5 after:bg-(--gray-12) after:animate-pulse",
           )}
         >
           {c.char === " " && c.status === "incorrect" ? "\u00B7" : c.char}
